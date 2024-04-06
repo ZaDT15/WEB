d6606c1e1fe8972161bfd2c3bf674c4667cd6d4e
ZaDT15 <155781559+ZaDT15@users.noreply.github.com>
2024-04-07 04:29:13 +0700

    Add files via upload

---
+++ process_login.php
@@ -0,0 +1,8 @@
+<?php
+// Proses login...
+// Jika login berhasil, arahkan pengguna ke halaman dashboard
+header("Location: dashboard.html");
+exit; // Pastikan tidak ada output lain sebelum header() digunakan
+?>
+
+
